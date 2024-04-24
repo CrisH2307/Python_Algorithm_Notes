a0343c887ed6b07a2ce5226d98c6e13d9422a1f2
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2024-04-24 23:17:41 +0000

    push

--- LinkedList/SinglyLinkedList.py
+++ LinkedList/SinglyLinkedList.py
@@ -21,3 +21,72 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+# How to Insert a Node a Front/Beginning
+'''
+_ To insert a node at the start/beginning/front:
+    + Make the first node of Linked List linked to the new node
+    + Remove the head from the original first node
+    + Make the new node as the head
+
+'''
+def push (self, new_data):
+    # 1 & 2: Allocate the Node and put in the data
+    new_node = Node(new_data)
+
+    # 3. Make next of new Node as head
+    new_node.next = self.head
+
+    # 4. Move the head to point to new Node
+    self.head = new_node
+
+
+# How to Insert a Node after a Given Node
+'''
+_ To insert a node after a given node:
+    + Check if the given node exists or not
+    + If it don't exist -> Terminate the process
+    + If the given node exist ->
+        1. Make the element to be inserted as a new node
+        2. Change the next pointer of a given node to the new node
+        3. Now shift the original next pointer of given node to the next pointer of new node
+
+'''
+def insertAfter(self, prev_node, new_data):
+    # 1. Check if the given prev_node exists
+    if prev_node is None:
+        print("The given prev node must in linked list")
+        return
+    # 2. Create new node and put in the data
+    new_node = Node(new_data)
+
+    # 3. Make next of new Node as next of prev_node
+    new_node.next = prev_node.next
+
+    # 4. Make next of prev_node as new_node
+    prev_node.next = new_node
+
+
+# How to Insert a Node at the End
+'''
+_ To insert a node at the end:
+   + Go to the last node
+   + Change the next pointer of last node from NULL to new node
+   + Make the next pointer of new node as NULL to show the end
+'''
+def append(self, new_data):
+    # 1. Create new node, put in the data and set next of None
+    new_node = Node(new_data)
+
+    # 2. If the linked list is empty, then make the new node as head
+    if self.head is None:
+        self.head = new_node
+        return
+    
+    # 3. Else traverse till the last node
+    last = self.head
+    while (last.next):
+        last = last.next
+    
+    # 4. Change the next of last node
+    last.next = new_node
