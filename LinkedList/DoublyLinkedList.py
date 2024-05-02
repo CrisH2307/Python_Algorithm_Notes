fe1103e1c51ae6d94c9392536265d8ae7678e554
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2024-05-01 22:14:17 -0400

    push

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -16,3 +16,46 @@ class Node:
         self.next = next
         self.prev = prev
         self.data = data
+
+'''
+Advantages:
++ Can be traversed in both forward and backward directions.
++ The delete operation in DLL is more efficient if a ptr to the node
+to be deleted is given
++ Quickly insert a new node before a given node
++ In SLL, to delete a node, a ptr to the previous is needed, to get the prev
+node, sometimes the list is traversed. But in DLL, we can get the prev node
+using the prev ptr.
+
+Disavantages:
++ Every node of DLL requires extra space for a prev ptr.
++ All operation require an extra ptr prev to be maintained.
+'''
+
+# Insert at the beginning
+'''
+1. Allocate memory for a new node and assign the provided value to its data field
+2. Set the prev ptr of the new_node to None
+3. If the list is empty: 
+    + Set the next ptr of the new_node to None
+    + Update the head ptr to point to the new_node
+4. If the list is not empty:
+    + Set the next ptr of the new_node to the current head
+    + Update the prev ptr of the current head to point to the new_node
+    + Update the head pointer to point to new_node
+'''
+def push(self, new_data):
+    # 1 & 2: Allocate the Node and put in the data
+    new_node = Node(data = new_data)
+
+    # 3. Make next of new node as head and previous as None
+    new_node.next = self.head
+    new_node.prev = None
+
+    # 4. Change prev of head node to new node
+    if self.head is not None:
+        self.head.prev = new_node
+    
+    # 5. Move the head to point to the new node
+    self.head = new_node
+    
