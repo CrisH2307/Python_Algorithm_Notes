3ba11c09230898e0d392b1ccba5716acee91a55b
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2024-05-31 12:47:13 -0400

    push

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -158,3 +158,20 @@ def append(self, new_data):
 
     # 6. Make last node as previous of new node
     new_node.prev = last
+
+
+# Reverse
+def reverse(self):
+    temp = None
+    current = self.head
+ 
+    # Swap next and prev for all nodes of doubly linked list
+    while current is not None:
+        temp = current.prev
+        current.prev = current.next
+        current.next = temp
+        current = current.prev
+ 
+    # Before changing head, check for the cases like empty list and list with only one node
+    if temp is not None:
+        self.head = temp.prev
